fd822e44177193c70383c734a0b3f73de2b60797
zhangchengyu <zhangchengyu@hosecloud.com>
2021-05-08 15:58:32 +0800

    添加kotlin语言

---
+++ src/main/java/com/example/demo/test/KtTestAll.kt
@@ -0,0 +1,13 @@
+package com.example.demo.test
+
+class KtTestAll{
+
+    fun fun0(){
+        println("hello,world!")
+    }
+
+}
+fun main(){
+    val ktTestAll = KtTestAll()
+    ktTestAll.fun0()
+}
--- src/main/java/com/example/demo/test/TestAll.java
+++ src/main/java/com/example/demo/test/TestAll.java
@@ -4,4 +4,8 @@ package com.example.demo.test;
  * 测试方法类
  */
 public class TestAll {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
 }
